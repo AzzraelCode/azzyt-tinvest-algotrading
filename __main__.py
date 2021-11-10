becadb9ca7e060f0326a13a9c0ebc620d2db5e75
Denis070 <d7dev@bk.ru>
2021-11-10 14:41:52 +0300

    Рыночные заявки в Песочнице через отложенные. Для видео https://youtu.be/B7jjyFu4CXE

--- __main__.py
+++ __main__.py
@@ -8,6 +8,13 @@ BBG0013HGFT4 - USDTOM
 """
 
 ti = Ti(use_sandbox=True)
+# print(ti.create_sandbox()) # не забудь записать ti_broker_account_id d creds/__init__.py в ti_broker_account_id и закоментить
+
+# узнать номер счета, или по ссылке https://www.tinkoff.ru/invest/portfolio/<набор цифр = номер broker_account_id>/
+# print(ti.get_sync_client().get_accounts().payload.accounts)
+ti.buy_pseudo(10, "BBG004730N88")
+print(ti.get_sync_client().get_portfolio(ti.broker_account_id))
+print(ti.get_sync_client().get_portfolio_currencies(ti.broker_account_id))
 
 # ti.buy(10, "BBG004730N88")
 # ti.sell(5, "BBG004730N88")
@@ -17,6 +24,3 @@ ti = Ti(use_sandbox=True)
 
 # print(ti.get_sync_client().get_market_search_by_ticker("SBER"))
 # print(ti.get_sync_client().get_market_orderbook("BBG004730N88", 2))
-
-sheet = Sheets()
-sheet.write(ti)
--- classes/ti.py
+++ classes/ti.py
@@ -12,14 +12,11 @@ class Ti:
     """
 
 
-    def __init__(self, use_sandbox = True):
+    def __init__(self, use_sandbox=False):
         self._use_sandbox = use_sandbox
-        # пока этот код использовать в реале не планируется (только песочница)
-        # однако минимальную подготовку по токенам сделал
-        self._key = creds.ti_sanbox_token if use_sandbox else creds.ti_real_token
-
+        self._key =  creds.ti_sanbox_token if use_sandbox else creds.ti_real_token
         self.sync_client = None
-        self.broker_account_id = None
+        self.broker_account_id = creds.ti_broker_account_id
 
     def get_sync_client(self):
         """
@@ -27,34 +24,12 @@ class Ti:
         https://tinkoffcreditsystems.github.io/invest-openapi/rest/
         :return:
         """
-        if not self.sync_client: self.sync_client = tinvest.SyncClient(self._key, use_sandbox=self._use_sandbox)
+        if not self.sync_client:
+            self.sync_client = tinvest.SyncClient(self._key, use_sandbox=self._use_sandbox)
+
         return self.sync_client
 
-    def get_broker_account_id(self):
-        """
-        Полдучаю broker_account_id для Песочницы
-        :return:
-        """
-        # проверка на работу в песочнице
-        if not self._use_sandbox: raise Exception("Я пока ещё только учусь и работаю только в Песочнице")
-
-        # получаю список аккаунтов (в песочнице только 1 акк)
-        if not self.broker_account_id:
-            accounts = self.get_sync_client().get_accounts().payload.accounts
-            self.broker_account_id = self.__create_sandbox() if len(accounts) <= 0 else accounts[0].broker_account_id
-
-        return self.broker_account_id
-
-    def delete_sandbox(self):
-        """
-        Удаление песочницы
-        :return:
-        """
-        accounts = self.get_sync_client().get_accounts().payload.accounts
-        # если аккаунт в песочнице уже создан, то удлаяю его (хотя есть и методы очистки)
-        if len(accounts) > 0: self.get_sync_client().remove_sandbox_account(accounts[0].broker_account_id)
-
-    def __create_sandbox(self):
+    def create_sandbox(self):
         """
         Создание песочницы
         https://tinkoffcreditsystems.github.io/invest-openapi/env/
@@ -62,6 +37,15 @@ class Ti:
         и ввод на акк виртуальных денег
         :return:
         """
+        # проверка на работу в песочнице
+        if not self._use_sandbox: return
+
+        # получаю список аккаунтов (в песочнице только 1 акк)
+        accounts = self.get_sync_client().get_accounts().payload.accounts
+
+        # если аккаунт в песочнице уже создан, то удлаяю его (хотя есть и методы очистки)
+        if len(accounts) > 0: self.get_sync_client().remove_sandbox_account(accounts[0].broker_account_id)
+
 
         # создаю аккаунт в песочнице и получаю его номер
         broker_account_id = self.get_sync_client().register_sandbox_account(
@@ -80,6 +64,35 @@ class Ti:
 
         return broker_account_id
 
+    def __market_order_pseudo(self, direction : tinvest.OperationType, lots : int, figi : str):
+        # https://tinkoffcreditsystems.github.io/invest-openapi/swagger-ui/#/market/get_market_orderbook
+        r = self.get_sync_client().get_market_orderbook(figi, 2).payload
+        # print(r)
+
+        # todo: Проверять объемы (quantity) и trade_status. Наличие бумаги (для шортов) и много ещё чего ;) ....
+        price = float(r.asks[0].price if direction == tinvest.OperationType.buy else r.bids[0].price)
+        print("%s %d lots of %s at price = %f, total spent = %f" % (direction, lots, figi, price, price*lots))
+        return self.__limit_order(direction, price, lots, figi )
+
+    def buy_pseudo(self, lots=1, figi='BBG0013HGFT4'):
+        """
+        Эмулирую рыночную ПРОДАЖУ для Песочницы, т.к. рыночная в песочнице ничего не знает о рыночной цене,
+        см. описание __market_order
+        :param lots:
+        :param figi:
+        :return:
+        """
+        self.__market_order_pseudo(tinvest.OperationType.buy, lots, figi)
+
+    def sell_pseudo(self, lots=1, figi='BBG0013HGFT4'):
+        """
+        Эмулирую рыночную ПРОДАЖУ для Песочницы, т.к. рыночная в песочнице ничего не знает о рыночной цене,
+        см. описание __market_order
+        :param lots:
+        :param figi:
+        :return:
+        """
+        self.__market_order_pseudo(tinvest.OperationType.sell, lots, figi)
 
     def __market_order(self, direction : tinvest.OperationType, lots : int, figi : str):
         """
